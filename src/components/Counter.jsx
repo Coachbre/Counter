373cf601cefb8d75dc3ef0358ee9b1e2b0f17e25
Coachbre <coachbre.dev@gmail.com>
2023-11-20 15:21:48 -0500

    counter is now active

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,23 @@
 // created using rafce
 import { Button } from 'react-bootstrap';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div style={{margin: "4rem"}}>
-      <h1 style={{marginBottom: "2rem"}}>0</h1>
-     <Button variant='success' style={{marginRight: "1rem"}}>+1</Button>
-     <Button variant='danger'>-1</Button>
+      <h1 style={{marginBottom: "2rem"}}>{count}</h1>
+      <Button variant='success' style={{marginRight: "1rem"}} 
+        onClick={() => setCount(count + 1)}>
+        +1
+      </Button>
+      
+     <Button variant='danger'onClick={() => setCount(count -1)}>
+        -1
+      </Button>
     </div>
   )
 };
-
+//count is initially set to 0. +1 button has an onclick to setcount by adding 1
 export default Counter;
