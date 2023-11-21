0786be972d665217f759ae1702ccc85afba2ddd8
Coachbre <coachbre.dev@gmail.com>
2023-11-21 15:52:56 -0500

    modularized fuctionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,34 @@
 // created using rafce
-import { Button } from 'react-bootstrap';
+import CustomButton from './Button';
 import React, { useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const addOne = () => {
+    setCount(count + 1);
+  }
+
+  const subtractOne = () => {
+    setCount(count -1);
+  }
+
   return (
     <div style={{margin: "4rem"}}>
       <h1 style={{marginBottom: "2rem"}}>{count}</h1>
-      <Button variant='success' style={{marginRight: "1rem"}} 
-        onClick={() => setCount(count + 1)}>
+
+      <CustomButton 
+        variant="success" 
+        style={{marginRight: "1rem"}}
+        onClick={addOne}>
         +1
-      </Button>
-      
-     <Button variant='danger'onClick={() => setCount(count -1)}>
+      </CustomButton>
+
+      <CustomButton variant="danger" 
+      onClick={subtractOne}>
         -1
-      </Button>
+        </CustomButton>
+
     </div>
   )
 };
